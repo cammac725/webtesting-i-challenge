396f2112253a2ebaaec4829ee0e21881ffa0fd53
cammac <camm@rocktechcomputer.com>
2019-05-20 16:15:04 -0700

    added tests and helpers

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,80 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item && typeof item === 'object') {
+    const objectProperties = Object.getOwnPropertyNames(item)
+    const objectNames = ['name', 'durability', 'enhancement']
+    if (arraysEqual(objectProperties, objectNames)) {
+      const enhanced = item.enhancement < 20 ? item.enhancement + 1 : item.enhancement
+      const newItem = {
+        name: item.name,
+        durability: item.durability,
+        enhancement: enhanced
+      }
+      return newItem
+    }
+  } else {
+    return null
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item && typeof item === 'object') {
+    const objectProperties = Object.getOwnPropertyNames(item)
+    const objectNames = ['name', 'durability', 'enhancement']
+    if (arraysEqual(objectProperties, objectNames)) {
+      if (item.enhancement > 16) {
+        return {
+          ...item,
+          enhancement: item.enhancement - 1,
+          durability: item.durability - 10
+        }
+      } else if (item.enhancement >= 15) {
+        return {
+          ...item,
+          durability: item.durability - 10
+        }
+      } else {
+        return {
+          ...item,
+          durability: item.durability - 5
+        }
+      }
+    }
+  } else {
+    return null
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  if (item && typeof item === 'object') {
+    const objectProperties = Object.getOwnPropertyNames(item)
+    const objectNames = ['name', 'durability', 'enhancement']
+    if (arraysEqual(objectProperties, objectNames)) {
+      const newItem = {
+        name: item.name,
+        durability: 100,
+        enhancement: item.enhancement
+      }
+      return newItem
+    }
+  } else {
+    return null
+  }
 }
+
 
 function get(item) {
   return { ...item };
+}
+
+function arraysEqual(a, b) {
+  if (a === b) return true;
+  if (a == null || b == null) return false;
+  if (a.length != b.length) return false;
+
+  for (var i = 0; i < a.length; ++i) {
+    if (a[i] !== b[i]) return false;
+  }
+  return true;
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,122 @@
 const enhancer = require('./enhancer.js');
-// test away!
+const { repair, fail, succeed, get } = require('./enhancer')
+
+describe('enhancer.js', () => {
+
+  describe('repair()', () => {
+    it('should return item with durability at 100', () => {
+
+      const item = {
+        name: 'The Sword',
+        durability: 85,
+        enhancement: 15
+      }
+
+      //arrange
+      const expected = {
+        name: 'The Sword',
+        durability: 100,
+        enhancement: 15
+      }
+
+      // act
+      const repairedItem = repair(item);
+
+      // assert
+      expect(repairedItem).toEqual(expected)
+    })
+
+    it('should return null when no args passed', () => {
+      const expected = null
+
+      const actual = repair();
+
+      expect(actual).toEqual(expected)
+    })
+
+    it('should return null or undefined when not passed a valid object', () => {
+      const invalidItem = {
+        name: "Shield",
+        speed: 44,
+        enhancement: 15
+      }
+
+      expect(repair(invalidItem)).toBeUndefined();
+    })
+
+  })
+
+  describe('succeed', () => {
+    it('should increase the enhancement by 1', () => {
+      const item = {
+        name: 'The Sword',
+        durability: 85,
+        enhancement: 14
+      }
+
+      //arrange
+      const expected = {
+        name: 'The Sword',
+        durability: 85,
+        enhancement: 15
+      }
+
+      // act
+      const enhancedItem = succeed(item)
+
+      // assert
+      expect(enhancedItem).toEqual(expected)
+    })
+
+    it('should return undefined when called with no args', () => {
+      expect(succeed()).toBeNull()
+    })
+  })
+
+  describe('fail', () => {
+    it('should decrease durability by 5 if the enhance is less than 15', () => {
+      const failItem = {
+        name: 'The Sword',
+        durability: 85,
+        enhancement: 13
+      }
+
+      const returnedItem = {
+        name: 'The Sword',
+        durability: 80,
+        enhancement: 13
+      }
+
+      expect(fail(failItem)).toEqual(returnedItem)
+    })
+
+    it('should decrease durability by 10 if enhance is greater than 15', () => {
+      const failItem = {
+        name: 'The Sword',
+        durability: 85,
+        enhancement: 15
+      }
+      const returnedItem = {
+        name: 'The Sword',
+        durability: 75,
+        enhancement: 15
+      }
+
+      expect(fail(failItem)).toEqual(returnedItem)
+    })
+
+    it('should decrease enhancement by 1 if existing enhancement is > 16', () => {
+      const failItem = {
+        name: 'The Sword',
+        durability: 85,
+        enhancement: 19
+      }
+      const returnedItem = {
+        name: 'The Sword',
+        durability: 75,
+        enhancement: 18
+      }
+      expect(fail(failItem)).toEqual(returnedItem)
+    })
+  })
+})
